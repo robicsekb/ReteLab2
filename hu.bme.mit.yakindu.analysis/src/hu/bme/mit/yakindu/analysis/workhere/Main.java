83daee75b27724faa7634b0b325ab1c34ec88a5d
Robicsek Bence <robicsekbenya@gmail.com>
2021-03-14 02:04:36 +0100

    2-es feladat

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -1,10 +1,15 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.util.ArrayList;
+
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 import org.yakindu.sct.model.sgraph.State;
 import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.model.sgraph.Transition;
+import org.yakindu.sct.model.sgraph.Vertex;
 
 import hu.bme.mit.model2gml.Model2GML;
 import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
@@ -29,7 +34,28 @@ public class Main {
 			EObject content = iterator.next();
 			if(content instanceof State) {
 				State state = (State) content;
-				System.out.println(state.getName());
+				EList<Transition> transList = state.getOutgoingTransitions();
+				ArrayList<Vertex> targetState = new ArrayList();
+				for (Transition t:transList) {
+					targetState.add(t.getTarget());
+				}
+				for (Vertex v:targetState) {
+					System.out.println(state.getName() + " -> " + v.getName());
+					if (v.getName() == null) {
+						for (Transition t:transList) {
+							EList<Transition> nonameTrans = t.getTarget().getIncomingTransitions();
+							for (Transition k:nonameTrans) {
+								if (k.getTarget().getName() == null) { 
+									System.out.println("Supposed name: " + state.getName() + " " + k.getSpecification());
+								}
+							}
+						}
+					}
+				}
+				if (transList.size() == 0)
+				{
+					System.out.println("Trapstate: " + state.getName());
+				}
 			}
 		}
 		
